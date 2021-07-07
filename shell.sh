f7680918aacecb435f8e1d00c180eddb3fd2d74a
Nic <nicolaipre@users.noreply.github.com>
2021-07-07 13:40:36 +0200

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+# docker exec -it idapro /bin/bash
 ssh -p 31337 -o StrictHostKeyChecking=no -o "UserKnownHostsFile /dev/null" -o ForwardX11=yes -o ForwardX11Trusted=yes -X root@localhost
